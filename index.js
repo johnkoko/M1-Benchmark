a7d6b1f374a9ce8cbc0399f0267e95f1ed9afca4
johnkoko <nkorjohn@gmail.com>
2021-10-01 15:20:19 +0100

    Started JS and re-organized the CSS and JS

---
+++ index.js
@@ -0,0 +1,22 @@
+// 21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
+        
+let x = "John";
+let y = "Doe";
+console.log(x +' <> ' + y)
+
+// 22) Create an object with properties such name, surname, email
+let myDetails =  {
+    name: "John",
+    surname: "Nkor",
+    email: "john@gmail.com",
+}
+console.log(myDetails)
+
+
+// 23) Delete the email property from the previously created object
+delete myDetails.email
+console.log(myDetails)
+
+
+// 24) Create an array with 10 strings in it
+
